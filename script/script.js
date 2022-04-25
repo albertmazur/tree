4277aee09b73edfa4ba878056bd115d1ec84d5dd
Albert Mazur <albert153@interia.pl>
2022-04-25 02:20:38 +0200

    Correct

--- script/script.js
+++ script/script.js
@@ -1,138 +1,140 @@
-const xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest()
 
-let li = document.querySelectorAll(".tree li");
+let li = document.querySelectorAll(".tree li")
 for (let liElement of li) {
     liElement.addEventListener("click", viewList, false)
 }
 
-let removeButton = document.querySelector(".removeFirst");
+let removeButton = document.querySelector(".removeFirst")
 if(removeButton!=null) removeButton.addEventListener("click", remove, false)
 
-let editButton = document.querySelector(".edit");
+let editButton = document.querySelector(".edit")
 if(editButton!=null) editButton.addEventListener("click", edit, false)
 
-let addButton = document.querySelector(".addFirst");
+let addButton = document.querySelector(".addFirst")
 if(addButton!=null) addButton.addEventListener("click", addDatabase, false)
 
 function viewList(e){
     if(this.lastChild.tagName!="UL" || this.lastChild==undefined){
-        xhr.open('POST', 'index.php?action=ajax');
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.open('POST', 'index.php?action=ajax')
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
         xhr.send('id=' + this.firstElementChild.dataset.id)
 
         xhr.onload = () => {
             if(xhr.status == 200){
-                let reslut = JSON.parse(xhr.response);
+                let reslut = JSON.parse(xhr.response)
                 if(reslut.length>0){
                     // Tworzenie listy
-                    let ul = document.createElement("ul");
-                    ul.dataset.id_rodzic=this.children[0].dataset.id;
+                    let ul = document.createElement("ul")
+                    ul.dataset.id_rodzic=this.children[0].dataset.id
                     for(let e of reslut){
                         //Tworzenie elemntu
-                        let li = document.createElement("li");
-                        li.addEventListener("click", viewList, false);
-                        let div = document.createElement("div");
-                        div.dataset.id = e.id;
-                        div.classList.add("list");
-                        div.innerText = e.nazwa;
+                        let li = document.createElement("li")
+                        li.addEventListener("click", viewList, false)
+                        let div = document.createElement("div")
+                        div.dataset.id = e.id
+                        div.classList.add("list")
+                        div.innerText = e.nazwa
 
-                        li.append(div);
-                        li.append(newElemRemove());
+                        li.append(div)
+                        li.append(newElemRemove())
                         li.append(createEditButton())
-                        ul.append(li);
+                        ul.append(li)
                     }
                     //Możliwośc dodawania do listy
                     ul.append(createAddButton())
-                    ul.dataset.have=true;
-                    this.append(ul);
+                    ul.dataset.have=true
+                    this.append(ul)
                 }
                 else {
-                    let ul = document.createElement("ul");
-                    ul.dataset.id_rodzic=this.firstElementChild.dataset.id;
+                    let ul = document.createElement("ul")
+                    ul.dataset.id_rodzic=this.firstElementChild.dataset.id
                     ul.append(createAddButton())
-                    ul.dataset.have=true;
-                    this.append(ul);
+                    ul.dataset.have=true
+                    this.append(ul)
                 }
             }
         }
     }
     else {
         if (this.lastChild.style.display === "none") this.lastChild.style.display = "flex"
-        else this.lastChild.style.display = "none";
+        else this.lastChild.style.display = "none"
     }
     e.stopPropagation();
 }
 //Tworzenie przysisku do dodawania
 function createAddButton(){
-    let li = document.createElement("li");
+    let li = document.createElement("li")
     li.addEventListener('click', viewList, false)
-    let button = document.createElement("button");
+    let button = document.createElement("button")
     button.classList.add("add")
     button.innerText="+"
     button.addEventListener("click", addDatabase, false)
-    li.append(button);
-    return li;
+    li.append(button)
+    return li
 }
 //Tworzenie nowego elemntu
 function newlist(){
     let div = document.createElement("div")
-    div.textContent = prompt("Nazwa:");
-    if(div.textContent==null) return null
-    div.classList.add("list");
+    div.textContent = prompt("Nazwa:")
+    if(div.textContent==='') return null
+    div.classList.add("list")
     return div;
 }
 
 function newElemRemove(){
     let button = document.createElement("button")
-    button.classList.add("remove");
-    button.className+=" btn btn-danger m-2";
-    button.innerText = "Usuń";
-    button.addEventListener("click", remove, false);
+    button.classList.add("remove")
+    button.className+=" btn btn-danger m-2"
+    button.innerText = "Usuń"
+    button.addEventListener("click", remove, false)
     return button;
 }
 
 //Dodwanie do bazdy
 function addDatabase(e){
-    this.after(createEditButton());
-    let remove = newElemRemove()
-    if(this.className=="addFirst") remove.className="removeFirst btn btn-danger m-2"
-    this.after(remove)
     let div = newlist()
-    this.after(div)
+    if(div!=null){
+        this.after(createEditButton())
+        let remove = newElemRemove()
+        if(this.className=="addFirst") remove.className="removeFirst btn btn-danger m-2"
+        this.after(remove)
+        this.after(div)
 
-    let id = this.parentElement.parentElement.dataset.id_rodzic
-    let id_prev
-    if(this.parentElement.parentElement.children.length-2>=0){
-        id_prev = this.parentElement.parentElement.children[this.parentElement.parentElement.children.length-2].firstElementChild.dataset.id
-    }
-    else id_prev = null
-    if(this.className!="addFirst"){
-        let button = createAddButton();
-        this.parentElement.parentElement.append(button)
-    }
-    this.remove();
+        let id = this.parentElement.parentElement.dataset.id_rodzic
+        let id_prev
+        if(this.parentElement.parentElement.children.length-2>=0){
+            id_prev = this.parentElement.parentElement.children[this.parentElement.parentElement.children.length-2].firstElementChild.dataset.id
+        }
+        else id_prev = null
+        if(this.className!="addFirst"){
+            let button = createAddButton()
+            this.parentElement.parentElement.append(button)
+        }
+        this.remove();
 
-    xhr.open('POST', 'index.php?action=add');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send('nazwa=' + div.textContent + "&id_rodzic=" +id+ "&id_prev=" +id_prev)
+        xhr.open('POST', 'index.php?action=add')
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        xhr.send('nazwa=' + div.textContent + "&id_rodzic=" +id+ "&id_prev=" +id_prev)
 
-    xhr.onload = () => {
-      if(xhr.status==200){
-          div.dataset.id=xhr.response;
-      }
+        xhr.onload = () => {
+          if(xhr.status==200){
+              div.dataset.id=xhr.response
+          }
+        }
     }
     e.stopPropagation()
 }
 
 function remove(e){
     if(confirm("Czy na pewno usunąć?")){
-        xhr.open('POST', 'index.php?action=remove');
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.open('POST', 'index.php?action=remove')
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
         xhr.send('id=' + this.previousSibling.dataset.id)
         if(this.classList.contains("removeFirst")){
-            let li = document.createElement("li");
+            let li = document.createElement("li")
             li.addEventListener('click', viewList, false)
-            let button = document.createElement("button");
+            let button = document.createElement("button")
             button.classList.add("addFirst")
             button.innerText="+"
             button.addEventListener("click", addDatabase, false)
@@ -140,7 +142,7 @@ function remove(e){
             document.querySelector(".tree ul").append(li)
         }
         xhr.onload = () =>{
-            if(xhr.status== 200) this.parentElement.remove();
+            if(xhr.status== 200) this.parentElement.remove()
         }
     }
     e.stopPropagation()
@@ -148,9 +150,9 @@ function remove(e){
 
 function createEditButton(){
     let button = document.createElement("button")
-    button.textContent="Edytuj";
+    button.textContent="Edytuj"
     button.className="btn btn-success"
-    button.addEventListener("click", edit, false);
+    button.addEventListener("click", edit, false)
     return button;
 }
 
@@ -163,29 +165,30 @@ function edit(e){
             document.querySelector("input[name=id_rodzic]").value != preElement.parentElement.parentElement.dataset.id_rodzic ||
             document.querySelector("input[name=id_prev]").value != preElement.parentElement.previousElementSibling.firstElementChild.dataset.id){
             alert("Zmiany nie zostaną zapisane")
+            window.location.reload(true)
         }
-        preElement.style.borderColor = 'salmon'
+        preElement.style.borderColor = 'black'
     }
 
-    let elem = this.parentElement.firstElementChild;
+    let elem = this.parentElement.firstElementChild
     elem.style.borderColor = 'red'
     document.querySelector("input[type=text]").value = elem.textContent
     document.querySelector("input[name=id]").value= elem.dataset.id
     preElement = elem
-    e.preventDefault()
+    e.stopPropagation()
 }
 
 document.getElementById("up").addEventListener("click", function (){
-    let li =  preElement.parentElement;
-    let parent = li.parentElement.parentElement.parentElement;
+    let li =  preElement.parentElement
+    let parent = li.parentElement.parentElement.parentElement
     if(parent.dataset.id_rodzic != undefined){
         if(li.nextElementSibling.firstElementChild.tagName==="DIV"){
-            let id_prev;
+            let id_prev
             if(li.previousElementSibling===null) id_prev=0
             else id_prev=li.previousElementSibling.firstElementChild.dataset.id
 
-            xhr.open('POST', 'index.php?action=up');
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.open('POST', 'index.php?action=up')
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
             xhr.send('id=' + li.nextElementSibling.firstElementChild.dataset.id + "&id_prev=" + id_prev)
 
             xhr.onload = () =>{
@@ -193,34 +196,35 @@ document.getElementById("up").addEventListener("click", function (){
             }
         }
 
-        parent.insertBefore(li, parent.lastChild);
+        parent.insertBefore(li, parent.lastChild)
 
         document.querySelector("input[name=id_rodzic]").value = parent.dataset.id_rodzic
         document.querySelector("input[name=id_prev]").value = parent.children[parent.children.length-3].firstElementChild.dataset.id
         document.querySelector("input[name=id_next]").value = null
     }
 
-}, false);
+}, false)
 
 document.getElementById("down").addEventListener("click", function (){
 
-    let li = preElement.parentElement;
+    let li = preElement.parentElement
     if(li.previousElementSibling!=null){
         let ul = li.previousElementSibling.lastElementChild
 
-        if(ul.tagName=="UL"){
+        if(ul.tagName==="UL"){
+            if(ul.style.display === "none") ul.style.display="flex"
             ul.insertBefore(li, ul.lastChild)
             document.querySelector("input[name=id_rodzic]").value = ul.dataset.id_rodzic
             document.querySelector("input[name=id_prev]").value = ul.children[ul.children.length-3].firstElementChild.dataset.id
             document.querySelector("input[name=id_next]").value = null
         }
-        else ("Aby przenieść otworz gałąż elementu z przodu");
+        else alert("Aby przenieść otworz gałąż elementu z przodu")
     }
 
 }, false)
 
 document.getElementById("left").addEventListener("click", function (){
-    let li =  preElement.parentElement;
+    let li =  preElement.parentElement
     if(li.previousElementSibling!=null){
         if(li.previousElementSibling.firstElementChild.tagName==="DIV"){
             li.parentElement.insertBefore(li, li.previousElementSibling)
@@ -234,7 +238,7 @@ document.getElementById("left").addEventListener("click", function (){
 }, false)
 
 document.getElementById("right").addEventListener("click", function (){
-    let li =  preElement.parentElement;
+    let li =  preElement.parentElement
     if(li.nextElementSibling!=null){
         if(li.nextElementSibling.firstElementChild.tagName==="DIV"){
             li.nextElementSibling.after(li)
--- templates/form.php
+++ templates/form.php
@@ -1,9 +1,9 @@
 <div class="col-4 ">
     <h1 class="text-center">Edytowanie elementu</h1>
     <form method="post" action="/?action=edit" >
-        <fieldset disabled>
+        <fieldset class="d-flex flex-column align-items-center" disabled>
             <div class="input-group"><span class="input-group-text">Nazwa</span> <input type="text" class="form-control" name="nazwa"></div>
-            <fieldset>
+            <fieldset class="d-flex flex-column align-items-center">
                 <button type="button" id="up" class="btn btn-secondary btn-lg m-2">&#8593;</button>
                 <div>
                     <button type="button" id="left" class="btn btn-secondary btn-lg m-2">&#8592;</button>
@@ -15,7 +15,7 @@
             <input type="hidden" name="id_rodzic">
             <input type="hidden" name="id_prev">
             <input type="hidden" name="id_next">
-            <input type="submit" class="btn btn-primary" value="Zapisz">
+            <input type="submit" class="btn btn-primary mt-4" value="Zapisz">
         </fieldset>
     </form>
 </div>
